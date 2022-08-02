f9b206434a326c9d9d49380b46313f80d99dc2a6
venzard <vasilij-ch@ya.ru>
2022-08-02 20:38:23 +0300

    fix: delete comments

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
   if(!matrix || matrix.length === 0){
     return [];
   }
   let arr = [];
-
-  for(let i =0; i < matrix.length; i++){
-    if ((i +1) % 2 === 0){
+  for(let i = 0; i < matrix.length; i++){
+    if ((i + 1) % 2 === 0){
       matrix[i] = matrix[i].reverse();
     }
     arr = arr.concat(matrix[i]);
   }
-  
-  console.log(arr);
   return arr;
 }
